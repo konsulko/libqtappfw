c4551774934cacb689127f2823f9025fbb20f3ab
Matt Porter <mporter@konsulko.com>
2018-06-19 14:56:26 -0400

    WIP models
    Change-Id: Ia4a02443e22e4a27dc974ef414b765667b26ff83
    Signed-off-by: Matt Porter <mporter@konsulko.com>

--- pbap.cpp
+++ pbap.cpp
@@ -14,23 +14,70 @@
  * limitations under the License.
  */
 
+#include <vcard/vcard.h>
+
 #include "message.h"
 #include "messageengine.h"
 #include "pbap.h"
 #include "pbapmessage.h"
 #include "responsemessage.h"
 
-Pbap::Pbap (QUrl &url, QObject * parent) :
+PhoneCall::PhoneCall(QString name, QString number, QString datetime, QString type)
+{
+    m_name = name;
+    m_number = number;
+    m_datetime = datetime;
+    m_type = type;
+}
+
+PhoneCall::~PhoneCall()
+{
+}
+
+Pbap::Pbap (QUrl &url, QQmlContext *context, QObject * parent) :
     QObject(parent),
     m_mloop(nullptr)
 {
     m_mloop = new MessageEngine(url);
+    m_context = context;
+    m_context->setContextProperty("CallHistoryModel", QVariant::fromValue(m_history));
     QObject::connect(m_mloop, &MessageEngine::messageReceived, this, &Pbap::onMessageReceived);
+#if 1
+    this->refreshContacts(50);
+#endif
 }
 
 Pbap::~Pbap()
 {
     delete m_mloop;
+}
+
+void Pbap::refreshContacts(int max_entries)
+{
+    PbapMessage *tmsg = new PbapMessage();
+    QJsonObject parameter;
+
+    if (max_entries >= 0)
+        parameter.insert("max_entries", max_entries);
+
+    tmsg->createRequest("contacts", parameter);
+    qInfo() << "sending contacts verbs with entries = " << max_entries;
+    m_mloop->sendMessage(tmsg);
+    tmsg->deleteLater();
+}
+
+void Pbap::refreshHistory(int max_entries)
+{
+    PbapMessage *tmsg = new PbapMessage();
+    QJsonObject parameter;
+
+    parameter.insert("list", "cch");
+    if (max_entries >= 0)
+        parameter.insert("max_entries", max_entries);
+
+    tmsg->createRequest("history", parameter);
+    m_mloop->sendMessage(tmsg);
+    tmsg->deleteLater();
 }
 
 void Pbap::search(QString number)
@@ -45,6 +92,66 @@ void Pbap::search(QString number)
     tmsg->createRequest("search", parameter);
     m_mloop->sendMessage(tmsg);
     tmsg->deleteLater();
+}
+
+void Pbap::updateContacts(QString vcards)
+{
+    QString name, number, type;
+
+    qInfo() << "contacts vcards: " << vcards;
+    QList<vCard> contacts_vcards = vCard::fromByteArray(vcards.toUtf8());
+
+    for (auto vcard : contacts_vcards) {
+        vCardProperty name_prop = vcard.property(VC_FORMATTED_NAME);
+        qInfo() << "Property name: " << name_prop.name();
+        qInfo() << "Property values: " << name_prop.values();
+//        qInfo() << "Property params: " << name_prop.params();
+        QStringList values = name_prop.values();
+        name = values.at(vCardProperty::DefaultValue);
+        qInfo() << "Name: " << name;
+        vCardProperty number_prop = vcard.property(VC_TELEPHONE);
+        if (number_prop.isValid()) {
+            QStringList values = number_prop.values();
+            number = values.at(0);
+            qInfo() << "Telephone: " << number;
+            vCardParamList params = number_prop.params();
+            type = params.at(0).value();
+            qInfo() << "Type: " << type;
+        }
+    }
+}
+
+#define VC_DATETIME "X-IRMC-CALL-DATETIME"
+
+void Pbap::updateHistory(QString vcards)
+{
+    QString name, number, datetime, type;
+//    QList<QObject *> m_history;
+
+    qInfo() << "history vcards: " << vcards;
+    QList<vCard> history_vcards = vCard::fromByteArray(vcards.toUtf8());
+
+
+    for (auto vcard : history_vcards) {
+        vCardProperty number_prop = vcard.property(VC_TELEPHONE);
+        if (number_prop.isValid()) {
+            QStringList values = number_prop.values();
+            name = number = values.at(0);
+            qInfo() << "Telephone: " << name;
+        }
+        vCardProperty datetime_prop = vcard.property(VC_DATETIME);
+        if (datetime_prop.isValid()) {
+            vCardParamList params = datetime_prop.params();
+            QStringList values = datetime_prop.values();
+            type = params.at(0).value();
+            datetime = values.at(0);
+            qInfo() << "Type: " << type;
+            qInfo() << "Date/Time: " << datetime;
+        }
+        m_history.append(new PhoneCall(name, number, datetime, type));
+    }
+
+    m_context->setContextProperty("CallHistoryModel", QVariant::fromValue(m_history));
 }
 
 void Pbap::sendSearchResults(QJsonArray results)
@@ -64,7 +171,11 @@ void Pbap::onMessageReceived(MessageType type, Message *msg)
     if (msg->isReply() && type == ResponseRequestMessage) {
         ResponseMessage *tmsg = qobject_cast<ResponseMessage*>(msg);
 
-        if (tmsg->requestVerb() == "search") {
+        if (tmsg->requestVerb() == "contacts") {
+            updateContacts(tmsg->replyData().value("vcards").toString());
+        } else if (tmsg->requestVerb() == "history") {
+            updateHistory(tmsg->replyData().value("vcards").toString());
+        } else if (tmsg->requestVerb() == "search") {
             sendSearchResults(tmsg->replyData().value("results").toArray());
         }
     }
--- pbap.h
+++ pbap.h
@@ -20,17 +20,51 @@
 #include <QDebug>
 #include <QObject>
 #include <QJsonArray>
+#include <QtQml/QQmlContext>
 
 #include "messageengine.h"
+
+class PhoneCall : public QObject
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QString name READ name NOTIFY nameChanged)
+    Q_PROPERTY(QString number READ number NOTIFY numberChanged)
+    Q_PROPERTY(QString datetime READ datetime NOTIFY datetimeChanged)
+    Q_PROPERTY(QString type READ type NOTIFY typeChanged)
+
+    public:
+        explicit PhoneCall(QString name, QString number, QString datetime, QString type);
+        virtual ~PhoneCall();
+
+        QString name() {return m_name;};
+        QString number() {return m_number;};
+        QString datetime() {return m_datetime;};
+        QString type() {return m_type;};
+
+    signals:
+        void nameChanged();
+        void numberChanged();
+        void datetimeChanged();
+        void typeChanged();
+
+    private:
+        QString m_name;
+        QString m_number;
+        QString m_datetime;
+        QString m_type;
+};
 
 class Pbap : public QObject
 {
     Q_OBJECT
 
     public:
-        explicit Pbap(QUrl &url, QObject * parent = Q_NULLPTR);
+        explicit Pbap(QUrl &url, QQmlContext *context, QObject * parent = Q_NULLPTR);
         virtual ~Pbap();
 
+        Q_INVOKABLE void refreshContacts(int max_entries);
+        Q_INVOKABLE void refreshHistory(int max_entries);
         Q_INVOKABLE void search(QString number);
 
     signals:
@@ -38,6 +72,11 @@ class Pbap : public QObject
 
     private:
         MessageEngine *m_mloop;
+        QQmlContext *m_context;
+        QMap<QString, QObject *>m_contacts;
+        QList<QObject *>m_history;
+        void updateContacts(QString);
+        void updateHistory(QString);
         void sendSearchResults(QJsonArray);
 
         // slots
